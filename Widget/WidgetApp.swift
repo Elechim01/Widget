0a64cf8424759e8a2b6d9f9bb904781756180e9f
Elechim01 <mikymanny015@gmail.com>
2021-02-24 22:03:25 +0100

    Initial Commit

---
+++ Widget/WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetApp.swift
+//  Widget
+//
+//  Created by Michele Manniello on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
